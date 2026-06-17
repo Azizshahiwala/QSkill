8edb93a976570862c12706ccd7056a781d1c72a0
Azizshahiwala <shahiwalaaziz8@gmail.com>
2026-06-17 13:33:08 +0530

    Basic task completed

--- VoiceActivatedPersonalAssistant/VoiceAssistant.py
+++ VoiceActivatedPersonalAssistant/VoiceAssistant.py
@@ -1,9 +1,11 @@
-import os,dotenv
+import os,dotenv,re
+from word2number import w2n 
+import threading,time
 import keyboard
 import pyttsx3 
 import requests
 import speech_recognition as sr
-
+from datetime import datetime,timedelta 
 class EnvNotFoundError(Exception):
     def __init__(self,error=".env file for keys not found.") -> None:
         super().__init__(error)
@@ -15,6 +17,7 @@ class VoiceAssistant:
     def __init__(self) -> None:
         self.APIS={"WEATHER_API_URL":"",
                    "WEATHER_API":"",
+                   "NEWS_API_URL":"",
                    "NEWS_API":""}
         self.mic_dict = {}
         self.recognizer = None
@@ -44,16 +47,70 @@ class VoiceAssistant:
             with open(self.path,"w+") as file:
                 file.write("WEATHER_API_URL=https://api.weatherapi.com/v1\n")
                 file.write("WEATHER_API=\n")
+                file.write("NEWS_API_URL=https://newsapi.org/v2/everything\n")
                 file.write("NEWS_API=")
                 print("An env file has been created. Make sure to set configurations.")
         except MicrophoneNotFoundError:
             pass
-    def showNews(self):
-        pass 
-    def showWeather(self,city="Ahmedabad"):
+    def processToNumber(self,captured_speech:str):
+        try:
+            processed_num = 0
+            cleaned = " ".join(self.processText(captured_speech))
+            print("cleaned",cleaned)
+            processed_num = float(w2n.word_to_num(cleaned))
+            return processed_num
+        except ValueError as e:
+            processed_num = re.findall(r"\d+",captured_speech)
+            return processed_num
+        except Exception as e:
+            print(e)
+        
+    def setReminder(self,task,delay=60):
+        print("Task:",task)
+        print("delay:",delay)
+        minute = 60//delay
+        self.speakMessage(f"Reminder set for {task}! I will remind you in {minute} minutes.")
+        time.sleep(delay)
+        self.speakMessage(f"Timer finished.")
+        print("Task completed")
+        
+    
+    def changeMicrophone(self):
+        if keyboard.is_pressed('m'):
+                print("-------")
+                print("To change microphone, input device_index")
+                device_index = int(input("device_index="))
+                if assistant.mic_dict[device_index]:
+                    print(f"Switching to {assistant.mic_dict[device_index]}")
+                    print("-------")
+                    sr.Microphone(device_index=device_index)
+                
+    def readNews(self,parameter="India",startDate=datetime.strftime(datetime.now()- timedelta(days=1),'%Y-%m-%d')):
         '''
-        This function fetches api response from url and gets fields.
+        This function will read at least 1 article.
         '''
+        call_url = f"{self.APIS['NEWS_API_URL']}?q={parameter}&from={startDate}&sortBy=publishedAt&apiKey={self.APIS['NEWS_API']}"
+        response = requests.get(call_url)
+        data = response.json()
+
+        article = data['articles'][0]
+
+        source = article['source']['name']
+        author = article['author']
+        title = article['title']
+        description = article['description']
+        publishedAt = article['publishedAt']
+        content = article['content']
+
+        self.speakMessage(f"Source: {source}"
+                          f"Title: {title}"
+                          f"Author: {author}"
+                          f"Published at: {publishedAt}"
+                          f"Description: {description}"
+                          f"Content: {content}")
+        
+
+    def readWeather(self,city="Ahmedabad"):
         call_url=rf"{self.APIS['WEATHER_API_URL']}/forecast.json?key={self.APIS['WEATHER_API']}&q={city}&days=1&aqi=yes&alerts=yes"
         response= requests.get(call_url)
         data = response.json()
@@ -74,6 +131,7 @@ class VoiceAssistant:
 
         alerts= data['alerts']['alert']
         print("alerts:",alerts)
+
         self.speakMessage(
     f"Weather in {curr_city}, {country}. "
     f"{atmosphere}, {temperature} degrees. "
@@ -104,7 +162,6 @@ class VoiceAssistant:
     def processText(self,text:str):
         cleaned=""
         for letter in text:
-            print(letter)
             if letter.isalnum() or letter.isspace():
                 cleaned += letter.lower()
         return cleaned.split()
@@ -113,13 +170,36 @@ class VoiceAssistant:
         cleaned_message = self.processText(text)
 
         if 'weather' in cleaned_message:
-            print("User wants to see weather")
-            self.showWeather()
+            self.speakMessage("Please wait, let me fetch weather data.")
+            self.readWeather()
         elif 'news' in cleaned_message:
-            print("User wants to see news")
-            self.showNews()
+            self.speakMessage("Please wait, let me fetch news data.")
+            self.readNews()
         elif 'reminder' in cleaned_message:
-            print("User wants to set reminder")
+            seconds=0
+            count=0
+            self.speakMessage("Ok, Speak task name.")
+            taskname = self.listenMessage()
+
+            self.speakMessage("Now speak number of minutes to remind you. Eg: one, three, two hundred, etc.")
+            captured_speech = self.listenMessage()
+
+            count = assistant.processToNumber(captured_speech)
+
+            if not count:
+                return
+            
+            print("Captured number: ",count)
+            if count or count >= 1 :
+                seconds = count*60
+            else:
+                seconds = 60
+                self.speakMessage("Could not understand minute count. timer will be set to 1 minute.")
+
+            thr = threading.Thread(target=self.setReminder, kwargs={"task":taskname,"delay":seconds})
+            thr.start()
+            thr.join()
+            self.speakMessage(f"Your timer for {count} minutes ended!")
         elif 'exit' in cleaned_message:
             self.speakMessage("Exitting... Thankyou for using!")
             exit(0)
@@ -129,11 +209,17 @@ class VoiceAssistant:
 if __name__ == "__main__":
     assistant = VoiceAssistant()
     assistant.speakMessage("Welcome to Voice Assistant. What would you like to do?")
-    assistant.speakMessage("A) Set a reminder. B) Check current weather. C) Check current news. D) Change microphone by pressing M.")
-
+    assistant.speakMessage("A) Set a reminder. B) Check current weather. C) Check current news.")
+    rate = assistant.engine.getProperty('rate')
+    
+    print("Speaking at rate: ",rate)
+    assistant.engine.setProperty('rate',150)
+    print("Listening...")
     while True:
         try:
             captured_speech = assistant.listenMessage()
+            if not captured_speech or captured_speech == '':
+                continue
             assistant.decideOperation(captured_speech)
         except IOError as e:
             print(e)
@@ -144,20 +230,3 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
             continue
-
-'''
-
-if keyboard.is_pressed('m'):
-                print("-------")
-                print("To change microphone, input device_index")
-                device_index = int(input("device_index="))
-                if assistant.mic_dict[device_index]:
-                    print(f"Switching to {assistant.mic_dict[device_index]}")
-                    print("-------")
-                    sr.Microphone(device_index=device_index)
-                continue
-
-                
-
-        
-'''
